914444d78153ed41b877b6703a17b30a00d99cb6
lcswillems <lcswillems@gmail.com>
2024-07-20 17:16:21 +0200

    Simplify - force one caller

--- behavior.test.ts
+++ behavior.test.ts
@@ -2,42 +2,36 @@ import { expect, test } from "vitest";
 import { FSWorld, e } from "xsuite";
 
 const egldUnit = 10n ** 18n;
-const callerWasmCodePath = "caller/output/caller.wasm"
-const calleeWasmCodePath = "callee/output/callee.wasm";
 
-for (let i = 1; i < 6; i++) {
-  for (let j = 1; j < 6; j++) {
-    test(`Test: ${i} caller and ${j} callee`, async () => {
-      using world = await FSWorld.start();
+for (let j = 6; j < 8; j++) {
+  test(`Test: ${j} callees`, async () => {
+    using world = await FSWorld.start();
 
-      const wallet = await world.createWallet({
-        address: { shard: 0 },
-        balance: egldUnit,
-      });
-      const callers = await world.createContracts(Array.from({ length: i }, () => ({
-        address: { shard: 0 },
-        code: `file:${callerWasmCodePath}`,
-        codeMetadata: [],
-      })))
-      const callees = await world.createContracts(Array.from({ length: j }, () => ({
-        address: { shard: 1 },
-        code: `file:${calleeWasmCodePath}`,
-        codeMetadata: [],
-      })))
-
-      await wallet.callContract({
-        callee: callers[0],
-        funcName: "x",
-        funcArgs: [e.List(...callers.map(v => e.Addr(v))), e.List(...callees.map(v => e.Addr(v)))],
-        gasLimit: 100_000_000,
-      });
-
-      for (const caller of callers) {
-        expect(await caller.getAccountKvs()).toEqual({})
-      }
-      for (const callee of callees) {
-        expect(await callee.getAccountKvs()).toEqual({})
-      }
+    const wallet = await world.createWallet({
+      address: { shard: 0 },
+      balance: egldUnit,
     });
-  }
+    const caller = await world.createContract({
+      address: { shard: 0 },
+      code: `file:caller/output/caller.wasm`,
+      codeMetadata: [],
+    });
+    const callees = await world.createContracts(Array.from({ length: j }, () => ({
+      address: { shard: 1 },
+      code: `file:callee/output/callee.wasm`,
+      codeMetadata: [],
+    })))
+
+    await wallet.callContract({
+      callee: caller,
+      funcName: "x",
+      funcArgs: [e.List(caller), e.List(...callees)],
+      gasLimit: 300_000_000,
+    });
+
+    expect(await caller.getAccountKvs()).toEqual({});
+    for (const callee of callees) {
+      expect(await callee.getAccountKvs()).toEqual({})
+    }
+  });
 }
